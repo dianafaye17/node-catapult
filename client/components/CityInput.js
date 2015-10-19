3a506607df7be478d8cdf650c16b7826b9b7d429
Diana <dianafaye17@gmail.com>
2015-10-19 14:51:33 -0500

    Added city greeting and added click listener on button

--- client/components/CityInput.js
+++ client/components/CityInput.js
@@ -1,21 +1,35 @@
 var m = require('mithril');
 var Location = require('../models/location');
 
+
 var CityInput = module.exports;
 
 CityInput.controller = function (options) {
   var ctrl = this;
   Location.sendCity();
+  this.city;
+  this.inputDirty = false;
 };
 
 CityInput.view = function (ctrl, options) {
   return m('.city-input-wrapper', [
     m('label', 'What City Do You Live In? ', [
-      m('input[type=text].city-input', { 
+      m('input[type=text].city-input', {
+        placeholder: 'Example:  Austin',
         oninput: function(e) { 
           ctrl.city = e.target['value'];
+          ctrl.inputDirty = true;
+          if (ctrl.inputDirty) {
+            ctrl.cityGreeting = 'You live in '+ ctrl.city + '!'
+          }
         }
-      }, 'Blargl')
-    ])
+      }),
+    ]),
+    m('button', {
+      onclick: function(e) {
+        console.log('clicked');
+      }
+    }, 'GO!'),
+    m('p', ctrl.cityGreeting)
   ]);
 };
