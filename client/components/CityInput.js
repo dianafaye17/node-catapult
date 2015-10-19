103fd958d335b9c8d31b5fd841dea270a958bb3f
Diana <dianafaye17@gmail.com>
2015-10-19 17:31:15 -0500

    Displaying weather conditions and temperature for input city

--- client/components/CityInput.js
+++ client/components/CityInput.js
@@ -8,20 +8,26 @@ CityInput.controller = function (options) {
   var ctrl = this;
   this.inputDirty = false;
 
-  this.printWeather = function() {
-    console.log('City sent:', ctrl.city);
-    Location.sendCity(ctrl.city)
-      .then(function (res) {
-        console.log('weather:', JSON.stringify(res));
-      })
-  }
-
   this.printCityGreeting = function(e) {
     ctrl.city = e.target['value'];
     ctrl.inputDirty = true;
     if (ctrl.inputDirty) {
       ctrl.cityGreeting = 'You live in '+ (ctrl.city || 'a magical land far, far away') + '!'
     }
+  }
+
+  this.getWeatherAttributes = function(weatherReport) {
+    console.log('name:', weatherReport.name)
+  }
+
+  this.printWeather = function() {
+    console.log('City sent:', ctrl.city);
+    Location.sendCity(ctrl.city)
+      .then(function (res) {
+        ctrl.report = res;
+        ctrl.weather = res["weather"][0]["main"];
+        ctrl.temp = res["main"]["temp"];
+      })
   }
 };
 
@@ -36,7 +42,9 @@ CityInput.view = function (ctrl, options) {
     m('button', {
       onclick: ctrl.printWeather
     }, 'GO!'),
-    m('p', ctrl.cityGreeting)
+    m('p', ctrl.cityGreeting),
+    m('p', JSON.stringify(ctrl.weather)),
+    m('p', JSON.stringify(ctrl.temp))
   ]);
 };
 
